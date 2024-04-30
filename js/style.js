907f48ad2dbf491d129d75b0efda4615f8dbc1c2
Paluoff <gabriel999pablo@gmail.com>
2024-04-30 19:47:28 -0300

    .

--- js/style.js
+++ js/style.js
@@ -209,17 +209,18 @@ document.addEventListener('keydown', event => {
 });
 
 
-function verificaFerramentasDev() {
-    if (window.devtools.open) {
-        // Redireciona para o site externo
-        window.location.href = "https://google.com";
+function bloquearCopia(Event) {
+    var Event = Event ? Event : window.event;
+    var tecla = (Event.keyCode) ? Event.keyCode : Event.which;
+    if (sessionStorage.getItem("ultimaTecla") === "17" && tecla === 85) {
+      Event.preventDefault();
+      window.location = "https://endereco-do-seu-site.com.br/conteudo-protegido.php";
     }
-}
+    sessionStorage.setItem("ultimaTecla", tecla);
+  }
 
-window.onload = verificaFerramentasDev;
-
-setInterval(verificaFerramentasDev, 1000);
-
+$(document).keypress(bloquearCopia);
+$(document).keydown(bloquearCopia);
 
 
 
